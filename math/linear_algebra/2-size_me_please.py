5d6b277e68c8dee3dddae0943aa19e8295355a6a
1Sultanz <z.r.amin40@gmail.com>
2025-08-07 19:14:55 +0000

    task

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-import numpy as np
-def matrix_shape(matrix):
-    return list(np.array(matrix).shape)
 
-mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))
+def matrix_shape(matrix):
+    if type(matrix[0]) is list:
+        return [len(matrix)] + matrix_shape(matrix[0])
+    else:
+        return [len(matrix)]
+
+
+if __name__ == "__main__":
+    mat1 = [[1, 2], [3, 4]]
+    print(matrix_shape(mat1))
+    mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+            [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+    print(matrix_shape(mat2))
