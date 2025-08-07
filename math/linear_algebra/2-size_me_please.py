f58aa97cd8e90ce4a8e60b7b4bbc2b84e188df4d
1Sultanz <z.r.amin40@gmail.com>
2025-08-07 19:27:04 +0000

    task

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Size Me Please"""
 
+
 def matrix_shape(matrix):
-    """function def matrix_shape(matrix): that calculates the shape of a matrix"""
+    """This function calculates the shape of a matrix"""
     if type(matrix[0]) is list:
         return [len(matrix)] + matrix_shape(matrix[0])
     else:
