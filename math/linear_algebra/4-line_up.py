2287a03f368abd3dfbb96a13e5519f7fe8605d70
1Sultanz <z.r.amin40@gmail.com>
2025-08-08 07:24:44 +0000

    task

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Line up"""
+
+
+def add_arrays(arr1, arr2):
+    """This function adds two arrays element-wise"""
+    arr = []
+    if len(arr1) == len(arr2):
+        for i in range(len(arr1)):
+            arr.append(arr1[i] + arr2[i])
+        return arr
+    else:
+        None
+        
+
+if __name__ == "__main__":
+    arr1 = [1, 2, 3, 4]
+    arr2 = [5, 6, 7, 8]
+    print(add_arrays(arr1, arr2))
+    print(arr1)
+    print(arr2)
+    print(add_arrays(arr1, [1, 2, 3]))
