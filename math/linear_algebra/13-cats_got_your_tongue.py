1119c9a604aeaa81a7b8f0c04e5b8ec061edf372
1Sultanz <z.r.amin40@gail.com>
2025-08-09 10:22:01 +0000

    task

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 """Cat's Got Your Tongue"""
+import numpy as np
+
+
+
 def np_cat(mat1, mat2, axis=0):
     """This function concatenates two matrices along a specific axis"""
     return np.cancatenate((mat1, mat2), axis)
