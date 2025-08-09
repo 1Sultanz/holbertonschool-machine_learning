9a9347fb0552e83bd46e4aece86aeca08c8dd975
1Sultanz <z.r.amin40@gail.com>
2025-08-09 13:14:52 +0000

    task

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Ridin Bareback"""
+"""Ridin Barebac"""
 
 
 def mat_mul(mat1, mat2):
     """This function performs matrix multiplication"""
-    if len(mat1[0]) == len(mat2):
-        mul = [sum(mat1[i][k] * mat2[k][j] for k in range(len(mat1[0])))
-                for i in range(len(mat1))
-                for j in range(len(mat2[0]))]
-        return mul
-    else:
-        None
+    if len(mat1[0]) != len(mat2):
+        return None
+    return [
+        [sum(mat1[i][k] * mat2[k][j] for k in range(len(mat1[0])))
+         for j in range(len(mat2[0]))]
+        for i in range(len(mat1))
+    ]
