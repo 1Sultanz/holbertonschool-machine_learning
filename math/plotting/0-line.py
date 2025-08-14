bf085748231632d4d0deddd000a43403d8818f95
1Sultanz <z.r.amin40@gmail.com>
2025-08-14 19:46:41 +0400

    task

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,13 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-
-    y = np.arange(0, 11) ** 3
-    plt.figure(figsize=(6.4, 4.8))
-
-    x = np.arange(0,11)
-    plt.plot(x, y, color = "red")
-    plt.xlim(0,10)
+    x = np.arange(0, 11)
+    y = x ** 3
+    plt.plot(x, y, color="red")  # color istifadə et
+    plt.xlim(0, 10)
     plt.show()
 
 line()
