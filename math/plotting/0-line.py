62f2d67a12e1d360edce8e1089a481d276e3b0ef
1Sultanz <z.r.amin40@gmail.com>
2025-08-14 19:42:57 +0400

    task

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -7,8 +7,9 @@ def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     
-    x = np.arange(0, 11)
+    x = np.arange(0,11)
     plt.plot(x, y, c = "red")
+    plt.xlim(0,10)
     plt.show()
 
 line()
