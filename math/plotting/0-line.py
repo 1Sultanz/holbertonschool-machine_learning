b7797ad2f7d9280eb74314f6202c54975d09ff61
1Sultanz <z.r.amin40@gmail.com>
2025-08-14 19:48:54 +0400

    task

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
+
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    x = np.arange(0, 11)              # X oxu 0-dan 10-a qədər
-    plt.plot(x, y, color="red")       # Qırmızı bərk xətt
-    plt.xlim(0, 10)                    # X-ox diapazonu 0–10
+    x = np.arange(0, 11)
+    plt.plot(x, y, c = "red")
     plt.show()
 
 line()
