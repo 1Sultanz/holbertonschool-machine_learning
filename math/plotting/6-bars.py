8c7c7742d9b763b5c6443e0ae0c71a1827ae3a42
1Sultanz <z.r.amin40@gmail.com>
2025-08-15 14:31:01 +0400

    task

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""Stacking Bars"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 def bars():
+    """Source code to plot a stacked bar graph"""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4,3))
     plt.figure(figsize=(6.4, 4.8))
