fd73208556b7120ff39a17491bfc207187cc739a
1Sultanz <z.r.amin40@gmail.com>
2025-08-15 16:29:31 +0400

    task

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -2,22 +2,15 @@
 """Derive happiness in oneself from a good day's work"""
 
 def poly_derivative(poly):
-    """this function calculates the derivative of a polynomial"""
-    if type(poly) != list:
-      return None
+    """Calculates the derivative of a polynomial"""
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+
+    if not all(isinstance(element, (int, float)) for element in poly):
+        return None
+
     if len(poly) == 1:
-      return [0]
-    if len(poly) == 0:
-        return None
-    for element in poly:
-      if type(element) != int and type(element) != float:
-        return None
-    if poly[0] == 0:
-      return [0]
-    
-    derivative = [poly[i] * i for i in range(1, len(poly))]
+        return [0]
 
-    if all(coef == 0 for coef in derivative):
-          return [0]
-
-    return derivative
+    # i = dərəcə, poly[i] * i = yeni əmsal
+    return [poly[i] * i for i in range(1, len(poly))]
