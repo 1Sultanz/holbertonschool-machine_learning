90dadf6a3ff048d3613305051190d34e0e48cb2e
1Sultanz <z.r.amin40@gmail.com>
2025-08-15 14:28:52 +0400

    task

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -8,21 +8,19 @@ def bars():
     plt.figure(figsize=(6.4, 4.8))
 
     people = ("Farrah", "Fred", "Felicia")
-    plt.bar(people, fruit[0], width=0.5, color="red", label="apples")
-    plt.bar(people, fruit[1], width=0.5, color="yellow", label="bananas",
-               bottom=fruit[0])
-    plt.bar(people, fruit[2], width=0.5, color="#ff8000", label="oranges",
-               bottom=fruit[0] + fruit[1])
-    plt.bar(people, fruit[3], width=0.5, color="#ffe5b4", label="peaches",
-               bottom=fruit[0] + fruit[1] + fruit[2])
-    
+    plt.bar(people, fruit[0], color="red", label="apples", width=0.5)
+    plt.bar(people, fruit[1], color="yellow", label="bananas",
+            bottom=fruit[0], width=0.5)
+    plt.bar(people, fruit[2], color="#ff8000", label="oranges",
+            bottom=fruit[0] + fruit[1], width=0.5)
+    plt.bar(people, fruit[3], color="#ffe5b4", label="peaches",
+            bottom=fruit[0] + fruit[1] + fruit[2], width=0.5)
 
     plt.ylabel('Quantity of Fruit')
     plt.yticks(range(0, 81, 10))
     plt.ylim(0, 80)
     plt.title("Number of Fruit per Person")
-
-    plt.legend(["apples", "bananas", "oranges", "peaches"])
+    plt.legend()
     plt.show()
 
 bars()
