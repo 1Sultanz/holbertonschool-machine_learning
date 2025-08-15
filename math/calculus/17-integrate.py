91ff30a9365cc4616461fec28b7ae45f4a56360f
1Sultanz <z.r.amin40@gmail.com>
2025-08-15 16:48:51 +0400

    task

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -8,6 +8,9 @@ def poly_integral(poly, C=0):
        len(poly) == 0 or not isinstance(C, (int, float))):
         return None
     
+    if all(coef == 0 for coef in poly):
+        poly = []
+
     integral = [poly[i]/i+1 for i in range(len(poly))]
 
     return [C] + [int(x) if x.is_integer() else x for x in integral]
