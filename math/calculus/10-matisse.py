e46af2d91f33903a7bab6fbd2fd16cc7269b5d5e
1Sultanz <z.r.amin40@gmail.com>
2025-08-15 16:20:25 +0400

    task

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 3
+"""Derive happiness in oneself from a good day's work"""
+
+def poly_derivative(poly):
+    """this function calculates the derivative of a polynomial"""
+    if type(poly) != list:
+      return None
+    if len(poly) == 1:
+      return [0]
+    if len(poly) == 0:
+        return None
+    for element in poly:
+      if type(element) != int and type(element) != float:
+        return None
+    if poly[0] == 0:
+      return [0]
+    
+    derivative = [poly[i] * i for i in range(1, len(poly))]
+
+    if all(coef == 0 for coef in derivative):
+          return [0]
+
+    return derivative
