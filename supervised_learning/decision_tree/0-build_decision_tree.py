6d828c717e74412133c02047360d2c1d14b51e43
1Sultanz <z.r.amin40@gmail.com>
2025-08-22 13:59:14 +0400

    task

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -31,28 +31,5 @@ class Leaf(Node):
         self.is_leaf = True
         self.depth = depth
 
-    def max_depth_below(self):
+    def max_depth_below(self) :
         return self.depth
-
-class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
-        self.rng = np.random.default_rng(seed)
-        if root:
-…    def depth(self) :
-        return self.root.max_depth_below()
-class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
-        self.rng = np.random.default_rng(seed)
-        if root:
-            self.root = root
-        else:
-            self.root = Node(is_root=True)
-        self.explanatory = None
-        self.target = None
-        self.max_depth = max_depth
-        self.min_pop = min_pop
-        self.split_criterion = split_criterion
-        self.predict = None
-
-    def depth(self) :
-        return self.root.max_depth_below()
