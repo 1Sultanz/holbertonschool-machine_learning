194d72aa58cdba8edd9b909a92334e34a6ff475e
1Sultanz <z.r.amin40@gmail.com>
2025-08-31 21:13:24 +0000

    task11

---
+++ supervised_learning/classification/11-neural_network.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""Neural Network"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """This class defines a neural network with one hidden
+     layer performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.nx = nx
+        self.nodes = nodes
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Weight getter for W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Bias getter for b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Activation getter for A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Weight getter for W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Bias getter for b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Activation getter for A2"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network"""
+        self.__A1 = 1/(1 + np.exp(-np.matmul(self.__W1, X) - self.__b1))
+        self.__A2 = 1/(1 + np.exp(-np.matmul(
+            self.__W2, self.__A1) - self.__b2))
+        return self.__A1, self.__A2
+
+   def cost(self, Y, A2):
+        """Calculates the cost of the neural network"""
+        m = Y.shape[1]
+        cost = (-1 / m) * np.sum(
+            Y * np.log(A2) + (1 - Y) * np.log(1.0000001 - A2))
+        return cost
