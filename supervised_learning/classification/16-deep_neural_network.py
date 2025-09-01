1c08fb86da4f45085a2f3cb3c95781746b120540
1Sultanz <z.r.amin40@gmail.com>
2025-09-01 09:48:55 +0000

    taskkk

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
+"""Deep Neural Network"""
+
 import numpy as np
 
+
 class DeepNeuralNetwork:
+    """This class defines a deep neural network performing binary classification"""
+
     def __init__(self, nx, layers):
+        """Class Constructor"""
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
-            raise TypeError("layers must be a list of positive integers")
+            raise TypeError("layers must be a positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        np.random.seed(0)  # test üçün sabit seed
+        np.random.seed(0)
         for i in range(self.L):
-            # Dövr içində həm tip, həm dəyər yoxlanır
             if not isinstance(layers[i], int) or layers[i] <= 0:
                 raise TypeError("layers must be a list of positive integers")
 
             prev_nodes = nx if i == 0 else layers[i - 1]
-            self.weights["W" + str(i + 1)] = np.random.randn(layers[i], prev_nodes) * np.sqrt(2 / prev_nodes)
+
+            self.weights["W" + str(i + 1)] = (
+                np.random.randn(layers[i], prev_nodes) * np.sqrt(2 / prev_nodes)
+            )
             self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
