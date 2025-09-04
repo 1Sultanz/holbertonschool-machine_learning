725ae23dd5120e74698c108186ab577efb006653
1Sultanz <z.r.amin40@gmail.com>
2025-09-04 13:03:43 +0000

    task

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""One-Hot Encode"""
+
+import numpy as np
+
+def one_hot_decode(one_hot):
+    """This function converts a one-hot matrix into a vector of labels"""
+    if type(one_hot) is not np.ndarray or one_hot.ndim != 2:
+        return None
+    return one_hot.transpose().argmax(axis=1)
