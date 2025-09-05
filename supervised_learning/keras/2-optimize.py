82d7438a8acc5f53cd5720aa90075af9ac878b20
1Sultanz <z.r.amin40@gmail.com>
2025-09-05 15:50:17 +0400

    task

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -7,7 +7,7 @@ def optimize_model(network, alpha, beta1, beta2):
     """This function sets up Adam optimization for
      a keras model with categorical crossentropy loss and accuracy metrics"""
     optimizer = K.optimizers.Adam(
-        alpha=alpha, beta_1=beta1, beta_2=beta2
+        learning_rate=alpha, beta_1=beta1, beta_2=beta2
     )
     network.compile(
         optimizer=optimizer,
