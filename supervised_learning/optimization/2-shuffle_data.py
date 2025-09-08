05b6e582ef3573b3043c2b4c796ed1d62b8edc0a
1Sultanz <z.r.amin40@gmail.com>
2025-09-08 14:04:06 +0400

    task

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -6,6 +6,8 @@ import numpy as np
 def shuffle_data(X, Y):
     """This function shuffles the data
      points in two matrices the same way"""
-    new_X = np.random.permutation(X)
-    new_Y = np.random.permutation(Y)
-    return new_X, new_Y
+    if len(X) != len(Y):
+        return None
+    else:
+        p = np.random.permutation(len(X))
+    return X[p], Y[p]
