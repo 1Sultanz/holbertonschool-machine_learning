02656f2ef4d726832a658887b300a82aaf769a54
1Sultanz <z.r.amin40@gmail.com>
2025-09-09 18:07:02 +0400

    task

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Momentum Upgraded"""
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1)
+    """This function sets up the gradient descent with
+    momentum optimization algorithm in TensorFlow"""
+    return tf.keras.optimizers.SGD(alpha, beta1)
