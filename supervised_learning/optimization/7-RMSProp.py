49fad365fba12e3e73a846802d79e91fb5a15d7e
1Sultanz <z.r.amin40@gmail.com>
2025-09-09 18:16:40 +0400

    task

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""RMSProp"""
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """This function updates a variable using
+     the RMSProp optimization algorithm"""
+    update_variable = beta2 * s + (1 - beta2) * grad**2
+    new_moment = var - (alpha / np.sqrt(s + epsilon)) * grad
+    return new_moment, update_variable
