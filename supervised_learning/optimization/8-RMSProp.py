d1a4270964bd60f32b5d61da50c162714fd06744
1Sultanz <z.r.amin40@gmail.com>
2025-09-09 18:33:58 +0400

    task

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""RMSProp Upgraded"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """This function sets up the RMSProp
+     optimization algorithm in TensorFlow"""
+    optimizer = tf.keras.optimizers.RMSprop(
+        alpha, beta2, epsilon
+    )
