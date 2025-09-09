7caa0c9e5f30c23ab7d58de5a5de9ec66c91e9a0
1Sultanz <z.r.amin40@gmail.com>
2025-09-09 17:59:32 +0400

    task

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Momentum"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """This function updates a variable using the
+     gradient descent with momentum optimization algorithm"""
+    update_variable = beta1 * v + (1 - beta1) * grad
+    new_moment = var - alpha * update_variable
+    return update_variable, new_moment
