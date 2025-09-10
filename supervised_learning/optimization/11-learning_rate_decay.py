300f4129c56e0229331bbc67d8e7e3cbe7bb2d72
1Sultanz <z.r.amin40@gmail.com>
2025-09-10 10:19:26 +0400

    task

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,9 @@
+#!/us/bin/env python3
+"""Learning Rate Decay0"""
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """This function updates the learning
+     rate using inverse time decay in numpy"""
+    new_alpha = alpha / (1 + decay_rate * (global_step // decay_step))
+    return new_alpha
