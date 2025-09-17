4b07176b8f71ebbd437dc0f0af08095c1e2375ce
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-09-17 11:19:59 +0400

    Create 1-sensitivity.py
    task

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Sensitivity"""
+import numpy as np
+
+
+def sensitivity(confusion):
+    """This function calculates the sensitivity for each class in a confusion matrix"""
+    classes = confusion.shape[0]
+    sensitivities = np.zeros(classes)
+
+    for i in range(classes):
+        TP = confusion[i][i]
+        FN = np.sum(confusion[i, :]) - TP
+        if TP + FN == 0:
+            sensitivities[i] = 0
+        else:
+            sensitivities[i] = TP / (TP + FN)
+    return sensitivities
