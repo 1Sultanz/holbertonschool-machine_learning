da7ac7870dc8d3d3c1425375220258e17cec5961
1Sultanz <z.r.amin40@gmail.com>
2025-09-21 09:00:42 +0000

    task

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 def l2_reg_cost(cost, model):
     """This function calculates the cost of
      a neural network with L2 regularization"""
-    reg_losses = tf.reduce_sum(model.losses)
-    total_cost = cost + reg_losses
+    reg_losses = [tf.reduce_sum(loss) for loss in model.losses]
+    total_cost = cost + tf.stack(reg_losses)
     return total_cost
