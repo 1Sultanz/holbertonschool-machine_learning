ea41ab32de6c99adf9a49c6f90ecd96793c51cf7
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-09-21 15:49:15 +0400

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -6,10 +6,12 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """This function creates a neural network layer in
      tensorFlow that includes L2 regularization"""
-    regularizer = tf.keras.regularizers.l2(lambtha)
+    def l2_regularizer(weights):
+        return lambtha * tf.reduce_sum(tf.square(weights))
+
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_regularizer=regularizer
+        kernel_regularizer=l2_regularizer
     )
     return layer(prev)
