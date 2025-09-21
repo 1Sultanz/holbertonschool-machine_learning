a833dec14a04178788d9591b1f0da157e6492d60
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-09-21 17:45:43 +0400

    Create 4-dropout_forward_prop.py

---
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pyton3
+"""Forward Propagation with Dropout"""
+import numpy as np
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """This function conducts forward
+     propagation using Dropout"""
+    cache = {"A0": X}
+    for i in range(1, L+1):
+        W = weights.get("W{}".format(i))
+        b = weights.get("b{}".format(i))
+        A_prev = cache.get("A{}".format(i-1))
+        Z = W @ A_prev + b
+        if i == L:
+            A = np.exp(Z) / np.sum(np.exp(Z),
+                                   axis=0,
+                                   keepdims=True)
+        else:
+            A = np.tanh(Z)
+            D = np.random.rand(*A.shape) < keep_prob
+            A = (A*D)/keep_prob
+            cache["D{}".format(i)] = D
+        cache["A{}".format(i)] = A
+    return cache
