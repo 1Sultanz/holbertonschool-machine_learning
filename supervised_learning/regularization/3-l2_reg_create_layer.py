5354173cbf0014faffc0d3eab118eef21b6beb12
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-09-21 15:31:36 +0400

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """This function creates a neural network layer in
      tensorFlow that includes L2 regularization"""
-    regularizer = tf.keras.regularizers.l2(lambtha)
+    regularizer = tf.keras.regularizers.L2(lambtha)
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
