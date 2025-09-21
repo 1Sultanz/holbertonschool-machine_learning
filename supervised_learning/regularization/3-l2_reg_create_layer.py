12869b5d96102678989f09348ee110e606288eef
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-09-21 15:39:20 +0400

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,12 +7,13 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     """This function creates a neural network layer in
      tensorFlow that includes L2 regularization"""
     initializer = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_in',
-                                                         distribution='untruncated_normal')
+                                                         distribution='normal')
     regularizer = tf.keras.regularizers.l2(lambtha)
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
         kernel_initializer=initializer,
         kernel_regularizer=regularizer
-    )(prev)
-    return layer
+    )
+
+    return layer(prev)
