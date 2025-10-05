adb9a078a2c036e0ea611d929a51c01557f820b8
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-10-05 16:56:36 +0400

    Update 3-pool_backward.py

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -4,32 +4,27 @@ import numpy as np
 
 
 def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """This function performs back propagation
-    over a pooling layer of a neural network"""
+    """This function  performs back propagation
+     over a pooling layer of a neural network"""
     m, h_new, w_new, c_new = dA.shape
     _, h_prev, w_prev, c = A_prev.shape
     kh, kw = kernel_shape
     sh, sw = stride
-    dA_prev = np.zeros_like(A_prev)
-
+    dA_prev = np.zeros((A_prev.shape))
     for e in range(m):
         for i in range(h_new):
             for j in range(w_new):
                 for k in range(c_new):
                     if mode == 'avg':
                         average_dA = dA[e, i, j, k] / (kh * kw)
-                        dA_prev[e,
-                                 i * sh:i * sh + kh,
-                                 j * sw:j * sw + kw,
-                                 k] += average_dA
+                        dA_prev[e, i * sh:i * sh + kh,
+                        j * sw:j * sw + kw,
+                        k] += average_dA
                     elif mode == 'max':
-                        a_slice = A_prev[e,
-                                         i * sh:i * sh + kh,
-                                         j * sw:j * sw + kw,
-                                         k]
-                        mask = (a_slice == np.max(a_slice))
-                        dA_prev[e,
-                                 i * sh:i * sh + kh,
-                                 j * sw:j * sw + kw,
-                                 k] += mask * dA[e, i, j, k]
+                        slice = A_prev[e, i * sh:i * sh + kh,
+                        j * sw:j * sw + kw, k]
+                        mask = (slice == np.max(slice))
+                        dA_prev[e, i * sh:i * sh + kh,
+                        j * sw:j * sw + kw,
+                        k] += mask * dA[e, i, j, k]
     return dA_prev
