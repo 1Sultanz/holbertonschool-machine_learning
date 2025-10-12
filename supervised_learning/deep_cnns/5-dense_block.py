1209a44230539063f4bc65095a5da1e5d55a12a5
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-10-12 22:56:45 +0400

    Create 5-dense_block.py

---
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Dense Block"""
+from tensorflow import keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    """This function builds a dense block"""
+    init = K.initializers.he_normal(seed=0)
+    i = 0
+    while i < layers:
+        layer = K.layers.BatchNormalization()(X)
+        layer = K.layers.Activation('relu')(layer)
+        layer = K.layers.Conv2D(filters=4*growth_rate,
+                                   kernel_size=1,
+                                   padding='same',
+                                   kernel_initializer=init,
+                                   )(layer)
+        layer = K.layers.BatchNormalization()(layer)
+        layer = K.layers.Activation('relu')(layer)
+        layer = K.layers.Conv2D(filters=growth_rate,
+                                   kernel_size=3,
+                                   padding='same',
+                                   kernel_initializer=init,
+                                   )(layer)
+        X = K.layers.concatenate([X, layer])
+        nb_filters += growth_rate
+        i += 1
+    return X, nb_filters
