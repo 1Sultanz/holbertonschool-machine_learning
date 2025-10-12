b68acd82e5f731876d23e791c2b597d41dbe4310
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-10-12 22:59:00 +0400

    Update 5-dense_block.py

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -10,18 +10,18 @@ def dense_block(X, nb_filters, growth_rate, layers):
     while i < layers:
         layer = K.layers.BatchNormalization()(X)
         layer = K.layers.Activation('relu')(layer)
-        layer = K.layers.Conv2D(filters=4*growth_rate,
-                                   kernel_size=1,
-                                   padding='same',
-                                   kernel_initializer=init,
-                                   )(layer)
+        layer = K.layers.Conv2D(filters=4 * growth_rate,
+                                kernel_size=1,
+                                padding='same',
+                                kernel_initializer=init,
+                                )(layer)
         layer = K.layers.BatchNormalization()(layer)
         layer = K.layers.Activation('relu')(layer)
         layer = K.layers.Conv2D(filters=growth_rate,
-                                   kernel_size=3,
-                                   padding='same',
-                                   kernel_initializer=init,
-                                   )(layer)
+                                kernel_size=3,
+                                padding='same',
+                                kernel_initializer=init,
+                                )(layer)
         X = K.layers.concatenate([X, layer])
         nb_filters += growth_rate
         i += 1
