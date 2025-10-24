2636d83cd5bf3d1e77f269761ce40b5ead0dd41e
1Sultanz <z.r.amin40@gmail.com>
2025-10-24 18:08:55 +0000

    update_task

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -6,11 +6,11 @@ import tensorflow.keras as K
 def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
     """This function uses the Yolo v3 algorithm to perform object detection"""
     self.model = K.models.load_model(model_path)
-    self.classes_names = []
+    self.class_names = []
     with open(classes_path, 'r') as f:
         for line in f:
             line = line.strip()
-            self.classes_names.append(line)
+            self.class_names.append(line)
     self.class_t = class_t
     self.nms_t = nms_t
     self.anchors = anchors
