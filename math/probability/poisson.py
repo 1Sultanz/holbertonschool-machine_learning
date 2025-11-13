05d44ba97b785b3f72a764749f2cd098cf56622f
1Sultanz <z.r.amin40@gmail.com>
2025-11-13 21:42:47 +0000

    task

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Initialize Poisson"""
+import numpy as np
+import math
 
 
 class Poisson:
@@ -16,3 +18,13 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Probability Mass Function: P(X = k)"""
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        e = 2.7182818285
+        pmf = ((e ** -self.lambtha) * self.lambtha ** k) / math.factorial(k)
+        return pmf
