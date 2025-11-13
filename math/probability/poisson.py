716a16419a2557c7112ab710806d38e408a1d29f
1Sultanz <z.r.amin40@gmail.com>
2025-11-13 21:27:19 +0000

    task

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -2,7 +2,7 @@
 """Initialize Poisson"""
 
 
-class poisson:
+class Poisson:
     """This class represent a poisson distribution"""
     def __init__(self, data=None, lambtha=1):
         """Class Constructor"""
@@ -13,6 +13,6 @@ class poisson:
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-            if data < 2:
+            if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
