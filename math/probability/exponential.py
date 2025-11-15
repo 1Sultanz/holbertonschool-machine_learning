fd00717b3d3e222ccbb3c5a6dcdd259be57506a9
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-11-15 09:33:02 +0400

    task

---
+++ math/probability/exponential.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Exponential Distribution"""
+
+
+class Exponential:
+    """This class represents an exponential distribution"""
+    def __init__(self, data=None, lambtha=1):
+        """Class constructor"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if data < 2:
+                raise ValueError("data must conatain multiple values")
+            self.lambtha = float(sum(data)/len(data))
