629db256dc0946c9ebdfe9717fc70b18d861be22
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-11-15 12:08:14 +0400

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -6,6 +6,7 @@ e = 2.7182818285
 
 class Normal:
     """This class represents a normal distribution"""
+
     def __init__(self, data=None, mean=0, stddev=1):
         """class constructor"""
         if data is None:
@@ -18,7 +19,7 @@ class Normal:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.mean = float(sum(data)/len(data))
+            self.mean = float(sum(data) / len(data))
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = float(variance ** 0.5)
 
@@ -34,3 +35,12 @@ class Normal:
         """Probabilit Density Function"""
         return (1 / (self.stddev * (2 * pi) ** 0.5)) * \
             e ** (-0.5 * self.z_score(x) ** 2)
+
+    @staticmethod
+    def error_f(x):
+        return ((2 / (pi) ** 0.5) *
+                (x - x ** 3 / 3 + x ** 5 / 10 - x ** 7 / 42 + x ** 9 / 216))
+
+    def cdf(self, x):
+        """Cumulative Distribution Function"""
+        return 0.5 * (1 + self.error_f(self.z_score(x) / (2 ** 0.5)))
