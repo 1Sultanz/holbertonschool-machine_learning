c2db15184842cce3c2e7b0d2163e81ed6f32920d
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-11-15 11:35:47 +0400

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -18,4 +18,12 @@ class Normal:
                 raise ValueError("data must contain multiple values")
             self.mean = float(sum(data)/len(data))
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
-            self.stddev = float(variance ** 0.5)
+            self.stddev = float(variance ** 0.5)\
+
+    def z_score(self, x):
+        """Normalize Normal"""
+        return (x - self.mean) / self.stddev
+    
+    def x_value(self, z):
+        """X value"""
+        return z * self.stddev + self.mean
