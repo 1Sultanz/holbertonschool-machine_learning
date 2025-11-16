c41bd3cb825b5e0165d1648f7c88f5c6eb19c404
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-11-16 17:53:47 +0400

    task

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -25,22 +25,35 @@ class Binomial:
             self.n = round(mean / self.p)
             self.p = float(mean / self.n)
 
+    @staticmethod
+    def factorial(num):
+        if num <= 1:
+            return 1
+        result = 1
+        for i in range(2, num + 1):
+            result *= i
+        return result
+
     def pmf(self, k):
         """Probability Mass Function for a given number of successes"""
         k = int(k)
         if k < 0 or k > self.n:
             return 0
 
-        def factorial(num):
-            if num <= 1:
-                return 1
-            result = 1
-            for i in range(2, num + 1):
-                result *= i
-            return result
-
-        binomial_coefficient = (factorial(self.n) /
-                                (factorial(k) * factorial(self.n - k)))
-        pmf_value = (binomial_coefficient * (self.p ** k)
+        binom = (self.factorial(self.n) /
+                                (self.factorial(k) * self.factorial(self.n - k)))
+        pmf = (binom * (self.p ** k)
                      * ((1 - self.p) ** (self.n - k)))
-        return pmf_value
+        return pmf
+
+    def cdf(self, k):
+        """Cumulative Distribution Function"""
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(0, k + 1):
+            binom = self.factorial(self.n) / (self.factorial(i) * self.factorial(self.n - i))
+            cdf += binom * (self.p ** i) * ((1 - self.p) ** (self.n - i))
+        return cdf
