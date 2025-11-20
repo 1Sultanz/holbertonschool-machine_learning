0647f69356aa0613e070d33dc0311d179fc1d804
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-11-21 00:25:46 +0400

    Create 3-adjugate.py

---
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""Adjugate"""
+
+
+def determinant(matrix):
+    """This function calculates the determinant of a matrix"""
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix[0]) == 0:
+        return 1
+
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for x in range(len(matrix)):
+        sub_matrix = [row[:x] + row[x + 1:] for row in matrix[1:]]
+        det += ((-1) ** x) * matrix[0][x] * determinant(sub_matrix)
+
+    return det
+
+
+def minor(matrix):
+    """This function calculates the minor of a matrix"""
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(matrix[0]):
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    minor = []
+    for r in range(len(matrix)):
+        i = []
+        for c in range(len(matrix[0])):
+            matrix_copy = [x[:] for x in matrix]
+            del matrix_copy[r]
+            for row in matrix_copy:
+                del row[c]
+            i.append(determinant(matrix_copy))
+        minor.append(i)
+
+    return minor
+
+
+def cofactor(matrix):
+    """This function calculates the cofactor of the matrix"""
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(sub_list):
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    n = len(matrix)
+    cof = []
+
+    for i in range(n):
+        row = []
+        for j in range(n):
+            sub = [r[:] for r in matrix]
+            del sub[i]
+            for r in sub:
+                del r[j]
+            sign = (-1) ** (i + j)
+            row.append(sign * determinant(sub))
+        cof.append(row)
+
+    return cof
+
+
+def adjugate(matrix):
+    """This function calculates the adjugate of a matrix"""
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(sub_list):
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    matrix = cofactor(matrix)
+    adj = [[matrix[i][j] for i in range(len(matrix))]
+           for j in range(len(matrix))]
+    return adj
