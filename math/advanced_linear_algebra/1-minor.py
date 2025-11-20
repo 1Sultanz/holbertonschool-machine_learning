9a7c934dd224376942ed9df44be99b4a89105509
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-11-20 15:50:06 +0400

    Update 1-minor.py

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Minor"""
+
+
+def determinant(matrix):
+    """This function calculates the determinant of a matrix"""
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix[0]) == 0:
+        return 1
+
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for x in range(len(matrix)):
+        sub_matrix = [row[:x] + row[x + 1:] for row in matrix[1:]]
+        det += ((-1) ** x) * matrix[0][x] * determinant(sub_matrix)
+
+    return det
+
+
+def minor(matrix):
+    """This function calculates the minor of a matrix"""
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 1:
+        return [[1]]
+    
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    minor = []
+    for r in range(len(matrix)):
+        i = []
+        for c in range(len(matrix[0])):
+            matrix_copy = [x[:] for x in matrix]
+            del matrix_copy[r]
+            for row in matrix_copy:
+                del row[c]
+            i.append(determinant(matrix_copy))
+        minor.append(i)
+
+    return minor
