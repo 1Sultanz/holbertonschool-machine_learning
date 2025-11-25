0115ba6904978b274dc78fb95a13f139ea928849
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-11-25 14:07:28 +0400

    Create 0-likelihood.py

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Likelihood"""
+import numpy as np
+
+
+def n_choose_k(n, k):
+    """Calculate combination C(n, k) without using math module"""
+    k = min(k, n - k)
+    result = 1
+    for i in range(1, k + 1):
+        result = result * (n - i + 1) // i
+    return result
+
+def likelihood(x, n, P):
+  """This function calculates the likelihood of obtaining this data given 
+  various hypothetical probabilities of developing severe side effects"""
+  if not isinstance(n, int) or n<=0:
+    raise ValueError("n must be a positive integer")
+  if not isinstance(x, int) or x<0:
+    raise ValueError("x must be an integer that is greater than or equal to 0") 
+  if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+  if x > n:
+        raise ValueError("x cannot be greater than n")
+  if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+  coeff = n_choose_k(n, x)
+  likelihoods = coeff * (P ** x) * ((1 - P) ** (n - x))
+
+  return likelihoods
