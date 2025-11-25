d7bf4ce01ce351c2c3f3a45931564eae17838838
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-11-25 14:24:29 +0400

    Update 0-likelihood.py

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -13,12 +13,13 @@ def n_choose_k(n, k):
 
 
 def likelihood(x, n, P):
-    """This function calculates the likelihood of obtaining this data given 
+    """This function calculates the likelihood of obtaining this data given
     various hypothetical probabilities of developing severe side effects"""
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if x > n:
