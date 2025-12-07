a8686ad46153e7e7621f3bebdaa9fd3eedce5ab0
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-12-07 20:31:52 +0400

    Create 0-pca.py

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Dimensinality Reduction"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """Compute the PCA, to get var% of the var explain"""
+    U, S, Vt = np.linalg.svd(X)
+    total_var_explain = 0
+    idx = 0
+    normal_S = S / np.sum(S)
+    for var_explain in normal_S:
+        total_var_explain += var_explain
+        idx += 1
+        if total_var_explain >= var:
+            break
+    return Vt.T[..., :idx]
