55a0bef0fa72eab9e7662c5ad2d8e4a46a82234d
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-12-07 20:40:00 +0400

    Create 1-pca.py

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Dimensinality Reduction"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """Compute the PCA"""
+    X = X - np.mean(X, axis=0)
+    _, __, Vt = np.linalg.svd(X)
+    return np.matmul(X, Vt.T[..., :ndim])
