63bbc8c77d06038fdaa3cf4ddd622a60c580bd04
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-12-14 23:02:13 +0400

    Update 4-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -6,8 +6,9 @@ GP = __import__('2-gp').GaussianProcess
 
 class BayesianOptimization:
     """Bayesian Optimization Class"""
+
     def __init__(self, f, X_init, Y_init, bounds, ac_samples,
-        l=1, sigma_f=1, xsi=0.01, minimize=True):
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """Init the class"""
         self.f = f
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
