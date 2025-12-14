7d396c2d3ffa8169d480b9c08eb74661bc19583f
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-12-14 22:55:27 +0400

    Create 4-bayes_opt.py

---
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Hyperparameter Tuning"""
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """Bayesian Optimization Class"""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+        l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """Init the class"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+        self.X_s = np.linspace(
+            bounds[0],
+            bounds[1],
+            ac_samples
+        ).reshape((-1, 1))
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """Acquisition"""
+        mu, sigma = self.gp.predict(self.X_s)
+        if self.minimize is False:
+            mu_sample_opt = np.amax(self.gp.Y)
+            imp = mu - mu_sample_opt - self.xsi
+        else:
+            mu_sample_opt = np.amin(self.gp.Y)
+            imp = mu_sample_opt - mu - self.xsi
+        with np.errstate(divide='warn'):
+            Z = imp / sigma
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        X_next = self.X_s[np.argmax(ei)]
+        return X_next, ei.reshape(-1)
