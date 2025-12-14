89def7d8cc19225d6005790a711696cbc0825b4c
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2025-12-14 23:05:19 +0400

    Update 5-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """Hyperparameter Tuning"""
 import numpy as np
-
+from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
     """Bayesian Optimization Class"""
-
     def __init__(self, f, X_init, Y_init, bounds, ac_samples,
                  l=1, sigma_f=1, xsi=0.01, minimize=True):
         """Init the class"""
