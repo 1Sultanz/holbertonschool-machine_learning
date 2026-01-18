8ce42805742c58b9ee0d5ba1ceda3a236df77c88
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2026-01-18 23:07:12 +0400

    Task

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -8,14 +8,14 @@ import numpy as np
 class RNNCell:
     """Cellule de RNN"""
     def __init__(self, i, h, o):
-        """initialisation"""
+        """initialization"""
         self.Wh = np.random.randn(i + h, h)
         self.bh = np.zeros((1, h))
         self.Wy = np.random.randn(h, o)
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
-        """propagation en avant"""
+        """Forward"""
         conc = np.concatenate((h_prev, x_t), axis=1)
         h_next = np.tanh(np.dot(conc, self.Wh)+self.bh)
         y = np.dot(h_next, self.Wy) + self.by
