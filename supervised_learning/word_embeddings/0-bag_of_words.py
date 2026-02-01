d5ca76a68db340912878cd7b3333f72a08666927
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2026-02-01 19:52:19 +0400

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
 """Bag Of Words"""
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 def bag_of_words(sentences, vocab=None):
     """Creates a bag of words embedding matrix"""
-    if vocab is None:
-        words = set()
-        for sentence in sentences:
-            for word in sentence.split():
-                words.add(word)
-        features = sorted(list(words))
-    else:
-        features = vocab
-    
-    word2idx = {word: idx for idx, word in enumerate(features)}
-    embeddings = np.zeros((len(sentences), len(features)), dtype=int)
-    
-    for i, sentence in enumerate(sentences):
-        for word in sentence.split():
-            if word in word2idx:
-                embeddings[i, word2idx[word]] += 1
-    
-    return embeddings, features
+    cont_vector = CountVectorizer(vocabulary=vocab)
+    embed = cont_vector.fit_transform(sentences)
+    features = cont_vector.get_feature_names_out()
+    return embed.toarray(), features
