1e2f40c6538b7deb47b545336f3f49e21bdbf36d
Sultanli Zamin <132377207+1Sultanz@users.noreply.github.com>
2026-02-01 20:15:31 +0400

    Update 2-word2vec.py

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -22,9 +22,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         epochs=epochs,
         workers=workers
     )
-    
     model.build_vocab(sentences)
     model.train(sentences, total_examples=model.corpus_count,
                 epochs=model.epochs)
-    
     return model
